be62927218a86a9a9ed18e43eb5d178dd1532d46
gug34 <gutionil1@gmail.com>
2026-01-23 17:42:40 +0100

    MOD Estilos finalizados

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,58 +3,64 @@
 @section('title', $product->name)
 
 @section('content')
-@vite(['resources/scss/pages/product.scss'])
-<main class="product-page-main">
-    <div class="product-page-container">
+    @vite(['resources/scss/pages/product.scss'])
+    <main class="product-page-main">
+        <div class="product-page-container">
 
-        <div class="product-detail">
+            <div class="product-detail">
 
-            <div class="product-image">
-                <img src="{{ asset('images/products/'.$product->image_url) }}" alt="{{ $product->name }}">
-            </div>
-
-            <div class="product-info">
-                <h1>{{ $product->name }}</h1>
-
-                @if($product->is_offer && $product->offer_percentage)
-                @php
-                $discounted = $product->price * (1 - $product->offer_percentage / 100);
-                @endphp
-                <p class="price">
-                    {{ number_format($discounted, 2) }}€
-                </p>
-                @else
-                <p class="price">{{ number_format($product->price, 2) }}€</p>
-                @endif
-
-                <p class="description">{{ $product->description }}</p>
-
-                <button class="btn-add-cart">Añadir al carrito</button>
-            </div>
-
-        </div>
-
-        {{-- COMENTARIOS --}}
-        <hr>
-
-        <div class="comments-section">
-            <h2>Opiniones de los usuarios</h2>
-
-            @forelse($product->comments as $comment)
-            <div class="comment-item">
-                <div class="comment-header">
-                    <span>{{ $comment->user->name ?? 'Usuario anónimo' }}</span>
-                    <span class="rating">{{ $comment->rating }}/5</span>
+                <div class="product-image">
+                    <img src="{{ asset('images/products/' . $product->image_url) }}" alt="{{ $product->name }}">
                 </div>
-                <p>{{ $comment->message }}</p>
-                <span class="comment-date">{{ $comment->created_at->format('d/m/Y') }}</span>
-            </div>
-            @empty
-            <p>No hay comentarios todavía.</p>
-            @endforelse
-        </div>
 
-    </div>
-</main>
+                <div class="product-info">
+                    <h1>{{ $product->name }}</h1>
+
+                    @if($product->is_offer && $product->offer_percentage)
+                        @php
+                            $discounted = $product->price * (1 - $product->offer_percentage / 100);
+                        @endphp
+                        <p class="price">
+                            {{ number_format($discounted, 2) }}€
+                        </p>
+                    @else
+                        <p class="price">{{ number_format($product->price, 2) }}€</p>
+                    @endif
+
+                    <p class="description">{{ $product->description }}</p>
+
+                    <div class="product-actions">
+                        <button class="btn-add-cart">Añadir al carrito <img src="{{ asset('images/icons/carrito.png') }}"
+                                alt="carrito"></button>
+
+                        <button class="btn-fast-buy"><img src="{{ asset('images/icons/fast-buy.png') }}"
+                                title="Comprar ahora" alt="carrito"></button>
+                    </div>
+                </div>
+
+            </div>
+
+            {{-- COMENTARIOS --}}
+            <hr>
+
+            <div class="comments-section">
+                <h2>Opiniones de los usuarios</h2>
+
+                @forelse($product->comments as $comment)
+                    <div class="comment-item">
+                        <div class="comment-header">
+                            <span>{{ $comment->user->name ?? 'Usuario anónimo' }}</span>
+                            <span class="rating">{{ $comment->rating }}/5</span>
+                        </div>
+                        <p>{{ $comment->message }}</p>
+                        <span class="comment-date">{{ $comment->created_at->format('d/m/Y') }}</span>
+                    </div>
+                @empty
+                    <p>No hay comentarios todavía.</p>
+                @endforelse
+            </div>
+
+        </div>
+    </main>
 
 @endsection
